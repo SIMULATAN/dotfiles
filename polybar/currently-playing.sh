c6d2c9ed6fc570541bd64debcee11ecd6fe64571
SIMULATAN <simulatan2319@gmail.com>
2022-11-16 16:05:34 +0100

    [polybar] fix "no player" output

--- polybar/currently-playing.sh
+++ polybar/currently-playing.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-player_status=$(playerctl status 2>/dev/null)
-
-artist="$(playerctl metadata artist)"
-title="$(playerctl metadata title)"
-
-[ "$player_status" = "Paused" ] && printf "⏸ "
+player_status="$(playerctl status 2>/dev/null)"
 
 if echo "$player_status" | grep -ixP "^(playing)|(paused)$" >/dev/null 2>&1; then
+    artist="$(playerctl metadata artist)"
+    title="$(playerctl metadata title)"
+
+    [ "$player_status" = "Paused" ] && printf "⏸ "
+
     printf "$artist"
     [ -n "$artist" ] && [ -n "$title" ] && printf " - "
     printf "$title"
