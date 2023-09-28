b666b8a2810a77f3cd16cf0eb455f9a19b558294
SIMULATAN <simulatan2319@gmail.com>
2023-09-28 08:09:43 +0200

    [profiles] dracula theme for less

---
+++ profiles/less_dracula.sh
@@ -0,0 +1,12 @@
+# https://draculatheme.com/man-pages
+
+export MANPAGER="/usr/bin/less -s -M +Gg"
+
+export LESS_TERMCAP_mb=$'\e[1;31m'      # begin bold
+export LESS_TERMCAP_md=$'\e[1;34m'      # begin blink
+export LESS_TERMCAP_so=$'\e[01;45;37m'  # begin reverse video
+export LESS_TERMCAP_us=$'\e[01;36m'     # begin underline
+export LESS_TERMCAP_me=$'\e[0m'         # reset bold/blink
+export LESS_TERMCAP_se=$'\e[0m'         # reset reverse video
+export LESS_TERMCAP_ue=$'\e[0m'         # reset underline
+export GROFF_NO_SGR=1                   # for konsole
