44f957f7599ccf012fedb255eebe27a8ef519d48
SIMULATAN <simulatan2319@gmail.com>
2024-03-19 10:44:57 +0100

    [zsh] ignore java distribution in version comparison

---
+++ profiles/space.sh
@@ -0,0 +1,20 @@
+function space() {
+  function warn() {
+    echo "[WARNING] $*"
+  }
+
+  # user will be warned if permissions aren't given
+  function hasPerms() {
+    test -r . || { warn "No permissions for this folder"; return 1; }
+    for file in ./*; do
+      test -r "$file" || { warn "No permissions for folder / file $file"; return 1; }
+    done
+    return 0
+  }
+
+  if ! hasPerms; then
+    warn "You don't have read permissions for some files in this folder!"
+  fi
+  # du with human output | filter empty | sort human output, reversed
+  du -h . --max-depth="${1:-1}" 2>/dev/null | grep -v '^0\s' | sort -hr
+}
