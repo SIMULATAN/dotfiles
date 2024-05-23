d873d5121eafc0d7a285e1d7e40c854f81913ec2
SIMULATAN <simulatan2319@gmail.com>
2024-05-23 09:23:27 +0200

    [profiles] fix dotfiles script
    Previously, SSH repos wouldn't open in the browser.
    This commit rewrites the script to be safer, more idiomatic and easier to read

--- profiles/dotfiles.sh
+++ profiles/dotfiles.sh
@@ -1,28 +1,37 @@
 function dotfiles() {
-  if [ "$1" != "" ]; then
-    INPUT=$(echo "$1" | tr '[:upper:]' '[:lower:]')
-    if [ "$INPUT" = "github" ] || [ "$INPUT" = "repo" ]; then
-      # run in subshell to prevent current shell from cd'ing into the dotfiles repo
-      # do in subshell to prevent CDing in current session
-      (
-        cd {{dotter.current_dir}} \ && export REPO="$(git config --get remote.origin.url)" \
-        && [[ ! "$REPO" =~ "^http" ]] && REPO="$(echo $REPO | sed 's/:/\//' | sed 's/git@/https:\/\//g')" || : \
-        && [ "$REPO" != "" ] \
-          && ((xdg-open "$REPO" &) && echo "Opened '$REPO' in your Browser.") \
-          || echo "No git repo found."
-      )
-      return 0
-    elif [ -d "{{dotter.current_dir}}/$1" ]; then
-      cd {{dotter.current_dir}}/$1
-      pwd
-    else
-      echo Directory \'$1\' not found.
-      return 1
-    fi
-  else
-    cd {{dotter.current_dir}}
+  if [ -z "$1" ]; then
+    cd "{{dotter.current_dir}}" || exit 1
   fi
-  # show all files, except for "." and ".." (current / upper directory)
-  ls -A
+
+  INPUT=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+  if [ "$INPUT" = "git" ] || [ "$INPUT" = "github" ] || [ "$INPUT" = "repo" ]; then
+    function open_github() {
+      cd "{{dotter.current_dir}}" || exit
+      REPO="$(git config --get remote.origin.url)"
+
+      # git over SSH
+      # replace `github.com:username` with `github.com/username` and `git@` with `https`
+      [[ ! "$REPO" =~ "^http" ]] && REPO="$(echo "$REPO" | sed 's/:/\//' | sed 's/git@/https:\/\//g')"
+
+      if [ "$REPO" != "" ]; then
+        xdg-open "$REPO" &
+        echo "Opened '$REPO' in your Browser."
+      else
+        echo "No git repo found."
+      fi
+    }
+
+    # run in subshell to prevent current shell from cd'ing into the dotfiles repo
+    (open_github)
+    return 0
+  elif [ -d "{{dotter.current_dir}}/$1" ]; then
+    cd "{{dotter.current_dir}}/$1" || exit 1
+    pwd
+    # show all files, except for "." and ".." (current / upper directory)
+    ls -A
+  else
+    echo "Directory '$1' not found."
+    return 1
+  fi
 }
 
