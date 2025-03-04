fcfc55bbe07094a1dd1a6bb2874c0e44e25390b8
SIMULATAN <jakob@simulatan.me>
2025-03-04 21:33:18 +0100

    feat(bin): `seal.sh`: cleanup output, support `.env`

--- bin/seal.sh
+++ bin/seal.sh
@@ -6,9 +6,10 @@ fi
 
 input_file="$1"
 file_name="${input_file/.local/}"
-output_file="$file_name"
-secret_name="${file_name/.yml/}"
-secret_name="${secret_name/.yaml/}"
+secret_name="${file_name/.env/}"
+secret_name="${secret_name/.env/}"
+secret_name="${secret_name/.yml/}"
+output_file="$secret_name.yml"
 
 if [[ -n "$2" ]]; then
   namespace_arg=(-n "$2")
@@ -16,8 +17,14 @@ fi
 
 secret="$(kubectl create secret generic "$secret_name" --dry-run=client --from-env-file="$input_file" -o json "${namespace_arg[@]}")"
 
-if echo "$secret" | kubeseal -w "$output_file"; then
-  echo "Successfully wrote secret to '$output_file'"
-else
+if ! echo "$secret" | kubeseal -w "$output_file"; then
   echo "Failed writing!"
+  exit 1
+fi
+
+echo "Successfully wrote secret to '$output_file'"
+
+if command -v yq &>/dev/null; then
+  secret="$(yq -P 'del(.metadata.creationTimestamp, .spec.template.metadata.creationTimestamp)' "$output_file")"
+  echo "$secret" | tail -n +2 > "$output_file"
 fi
