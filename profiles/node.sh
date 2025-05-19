d59dbdb45df6bfd405d26ec05b3e89b6a3e373b4
SIMULATAN <jakob@simulatan.me>
2025-05-19 11:10:04 +0200

    feat(profiles): set PNPM_HOME and add to PATH

--- profiles/node.sh
+++ profiles/node.sh
@@ -1,2 +1,8 @@
 command -v npm &>/dev/null || return 0
 export NODE_PATH="$(npm root -g)"
+
+export PNPM_HOME="${XDG_DATA_HOME}/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
