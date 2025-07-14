a1edc3f06f1cf2248cb1f7a650b70935722894d9
SIMULATAN <jakob@simulatan.me>
2025-07-14 12:44:47 +0200

    feat(profiles): improve Kubernetes diff formatting
    Removes bloat labels/annotations and enables colored diff output

---
+++ bin/kubernetes-diff.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Thanks to https://www.reddit.com/r/kubernetes/comments/i0zbcl/comment/fzzjnjt/
+
+deletes=(
+  '.metadata.annotations."deprecated.daemonset.template.generation"'
+  '.metadata.annotations."argocd.argoproj.io/tracking-id"'
+  '.metadata.annotations."kubectl.kubernetes.io/last-applied-configuration"'
+  '.metadata.annotations | select(length==0)'
+  '.metadata.labels."argocd.argoproj.io/instance"'
+  '.metadata.labels."kubectl.kubernetes.io/last-applied-configuration"'
+  '.metadata.labels | select(length==0)'
+)
+deletes=("${deletes[@]/#/del(}")
+deletes=("${deletes[@]/%/)}")
+
+function join_by { local IFS="$1"; shift; echo "$*"; } # https://stackoverflow.com/a/17841619/21038281
+delete="$(join_by '|' "${deletes[@]}")"
+
+for file in "$1"/* "$2"/*; do
+  yq -i "$delete" "$file" # clean up LIVE and MERGED files
+done |& grep -v '/*: no such file or directory' # https://stackoverflow.com/a/16321435/21038281
+
+diff_args=(
+  "--minimal"
+  "--width=120"
+  "-u"
+  "-N"
+)
+if [[ ! $ANSIBLE_MODE = YES ]]; then
+  diff_args+=("--color=always")
+fi
+
+diff "${diff_args[@]}" "$@" | awk '!/^diff/ {if ($1 ~ /(---|\+\+\+)/) {print $1, $2} else {print $0}}'
--- profiles/kubernetes.sh
+++ profiles/kubernetes.sh
@@ -1,3 +1,5 @@
 export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 
+export KUBECTL_EXTERNAL_DIFF=kubernetes-diff.sh
+
 alias kx="kubectl config use-context"
